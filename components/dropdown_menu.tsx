fb5dfeab63bc2d0b91ecc271b277e4807830b6c3
ebowwa <ec.arbee1@gmail.com>
2024-02-05 12:01:31 -0800

    added hrefs

--- components/dropdown_menu.tsx
+++ components/dropdown_menu.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { HamburgerMenuIcon, ChevronRightIcon, CheckIcon } from '@radix-ui/react-icons';
+import Link from 'next/link'; // Importing Link from next/link
 
 const SiteDirectoryMenu = () => {
   return (
@@ -19,40 +20,36 @@ const SiteDirectoryMenu = () => {
       <DropdownMenu.Portal>
         <DropdownMenu.Content
           className="min-w-[220px] bg-gray-100 rounded-lg p-4 shadow-sm border border-gray-200"
-
           sideOffset={5}
         >
-          {/* Simple items */}
-          <DropdownMenu.Item className="dropdown-item">Home</DropdownMenu.Item>
-          <DropdownMenu.Item className="dropdown-item">Pricing</DropdownMenu.Item>
-          <DropdownMenu.Item className="dropdown-item">Team</DropdownMenu.Item>
+          {/* Wrapped DropdownMenu.Item with Link */}
+          <Link href="/" passHref><DropdownMenu.Item className="dropdown-item">Home</DropdownMenu.Item></Link>
+          <Link href="/pricing" passHref><DropdownMenu.Item className="dropdown-item">Pricing</DropdownMenu.Item></Link>
+          <Link href="/team" passHref><DropdownMenu.Item className="dropdown-item">Team</DropdownMenu.Item></Link>
 
-          {/* Nested dropdown for Store section */}
+          {/* For nested dropdowns, consider using onClick handlers to navigate or keep as is if deeper navigation structure is intended */}
           <DropdownMenu.Sub>
             <DropdownMenu.SubTrigger className="dropdown-trigger">Store</DropdownMenu.SubTrigger>
             <DropdownMenu.SubContent className="dropdown-content">
-              <DropdownMenu.Item className="dropdown-item">All Products</DropdownMenu.Item>
-              <DropdownMenu.Item className="dropdown-item">New Arrivals</DropdownMenu.Item>
-              <DropdownMenu.Item className="dropdown-item">Best Sellers</DropdownMenu.Item>
+              <Link href="/store/all-products" passHref><DropdownMenu.Item className="dropdown-item">All Products</DropdownMenu.Item></Link>
+              <Link href="/store/new-arrivals" passHref><DropdownMenu.Item className="dropdown-item">New Arrivals</DropdownMenu.Item></Link>
+              <Link href="/store/best-sellers" passHref><DropdownMenu.Item className="dropdown-item">Best Sellers</DropdownMenu.Item></Link>
             </DropdownMenu.SubContent>
           </DropdownMenu.Sub>
 
-          {/* Nested dropdown for Documentation */}
+          {/* Documentation section with nested dropdown */}
           <DropdownMenu.Sub>
             <DropdownMenu.SubTrigger className="dropdown-trigger">Documentation</DropdownMenu.SubTrigger>
             <DropdownMenu.SubContent className="dropdown-content">
-              <DropdownMenu.Item className="dropdown-item">API Docs</DropdownMenu.Item>
-              <DropdownMenu.Item className="dropdown-item">Component Usage</DropdownMenu.Item>
+              <Link href="/documentation/api-docs" passHref><DropdownMenu.Item className="dropdown-item">API Docs</DropdownMenu.Item></Link>
+              <Link href="/documentation/component-usage" passHref><DropdownMenu.Item className="dropdown-item">Component Usage</DropdownMenu.Item></Link>
             </DropdownMenu.SubContent>
           </DropdownMenu.Sub>
 
-          {/* Account related links */}
-          <DropdownMenu.Item className="dropdown-item">Login</DropdownMenu.Item>
-          <DropdownMenu.Item className="dropdown-item">Sign Up</DropdownMenu.Item>
-
-          {/* Additional sections */}
-          <DropdownMenu.Item className="dropdown-item">Share</DropdownMenu.Item>
-          <DropdownMenu.Item className="dropdown-item">Affiliate Program</DropdownMenu.Item>
+          <Link href="/login" passHref><DropdownMenu.Item className="dropdown-item">Login</DropdownMenu.Item></Link>
+          <Link href="/signup" passHref><DropdownMenu.Item className="dropdown-item">Sign Up</DropdownMenu.Item></Link>
+          <Link href="/share" passHref><DropdownMenu.Item className="dropdown-item">Share</DropdownMenu.Item></Link>
+          <Link href="/affiliate-program" passHref><DropdownMenu.Item className="dropdown-item">Affiliate Program</DropdownMenu.Item></Link>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
