88c6236df85a055cde6221ba3f3878fcaadb245a
ebowwa <ec.arbee1@gmail.com>
2024-02-05 13:21:53 -0800

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,45 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Toaster } from "@/components/ui/toaster"; // Import the Toaster component
+import SupabaseProvider from './supabase-provider';
+
 
 const inter = Inter({ subsets: ['latin'] }) // Loading the Inter font with 'latin' subset
 
-export const metadata = {
+export const meta = {
   title: 'voiceovers',
   description: '"Enhance your digital content with Voiceovers – the premier AI voiceover application designed to revolutionize video productions. Powered by cutting-edge artificial intelligence, Voiceovers offers an intuitive platform for adding lifelike, customizable voiceovers to any video, making it perfect for content creators, marketers, and educators seeking to elevate their media presence. Our user-friendly interface ensures seamless integration of high-quality audio into your projects, transforming your videos with engaging narrations that captivate your audience. Unlock the potential of your videos with Voiceovers, where innovation meets creativity."',
+  cardImage: '/og.png',
+  robots: 'follow, index',
+  favicon: '/favicon.ico',
+  url: 'https://subscription-starter.vercel.app',
+  type: 'website'
 }
+
+export const metadata = {
+  title: meta.title,
+  description: meta.description,
+  cardImage: meta.cardImage,
+  robots: meta.robots,
+  favicon: meta.favicon,
+  url: meta.url,
+  type: meta.type,
+  openGraph: {
+    url: meta.url,
+    title: meta.title,
+    description: meta.description,
+    cardImage: meta.cardImage,
+    type: meta.type,
+    site_name: meta.title
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@vercel',
+    title: meta.title,
+    description: meta.description,
+    cardImage: meta.cardImage
+  }
+};
 
 export default function RootLayout({
   children,
@@ -18,8 +50,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SupabaseProvider>
         <main>{children}</main> {/* Main content area where child components will be rendered */}
         <Toaster /> {/* Placement of the Toaster component */}
+        </SupabaseProvider>
       </body>
     </html>
   )
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,14 @@
-// app/components/ui/Logo.tsx
 import React from 'react';
+import Link from 'next/link'; // Import Link from next/link
 
 const Logo: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <div className="absolute top-0 left-0 mt-4 ml-4">
-        <h1 className="text-xl font-bold">voiceovers</h1>
+        {/* Directly apply Link without wrapping it in an <a> tag */}
+        <Link href="/" className="text-xl font-bold" legacyBehavior>
+          voiceovers
+        </Link>
       </div>
     </div>
   );
